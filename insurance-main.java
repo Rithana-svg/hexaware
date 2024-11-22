6e254b314347a2b6be8fc84f28ae8ce801a06685
Rithana-svg <rithanamanickam@gmail.com>
2024-11-22 15:19:21 +0530

    Update insurance-main.java

--- insurance-main.java
+++ insurance-main.java
@@ -1,4 +1,4 @@
-// MainModule.java
+
 package mainmod;
 
 import dao.IPolicyService;
@@ -12,13 +12,13 @@ public class MainModule {
         IPolicyService policyService = new InsuranceServiceImpl();
 
         try {
-            // Create a new policy
+            
             Policy newPolicy = new Policy();
-            // Set policy details
+            
             boolean created = policyService.createPolicy(newPolicy);
             System.out.println("Policy created: " + created);
 
-            // Get policy by ID
+        
             try {
                 Policy policy = policyService.getPolicy(1);
                 System.out.println("Retrieved policy: " + policy);
@@ -26,21 +26,21 @@ public class MainModule {
                 System.out.println("Error: " + e.getMessage());
             }
 
-            // Get all policies
+            
             Collection<Policy> policies = policyService.getAllPolicies();
             System.out.println("All policies: " + policies);
 
-            // Update policy
+
             try {
                 Policy updatePolicy = new Policy();
-                // Set updated policy details
+                
                 boolean updated = policyService.updatePolicy(updatePolicy);
                 System.out.println("Policy updated: " + updated);
             } catch (PolicyNotFoundException e) {
                 System.out.println("Error: " + e.getMessage());
             }
 
-            // Delete policy
+        
             try {
                 boolean deleted = policyService.deletePolicy(1);
                 System.out.println("Policy deleted: " + deleted);
